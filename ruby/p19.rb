767a9814d0ae425fd438af882b2d47476f4628c0
Tyler Wymer <twymer@gmail.com>
2011-05-31 23:09:08 -0400

    comments

--- ruby/p19.rb
+++ ruby/p19.rb
@@ -1,10 +1,13 @@
+# A lot simpler than my python try.. didn't have to
+# use any personal checking of day of week or if it
+# is a leapyear
 require 'Date'
 
 count = 0
 
+# Date.civil defaults to first day of month
 1901.upto(2000) do |year|
   1.upto(12) do |month|
-    # Date.civil defaults to first day of month
     if Date.civil(year, month).wday == 0
       count += 1
     end
